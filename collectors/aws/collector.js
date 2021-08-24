0b4a8254a6b8506e5601a3276c432934df85745a
Anat Portnoy <anatportn@gmail.com>
2021-08-24 17:08:01 +0100

    SAAS-2420: Plugin to check that elastiCache not using default ports

--- collectors/aws/collector.js
+++ collectors/aws/collector.js
@@ -430,6 +430,12 @@ var calls = {
             paginate: 'NextToken'
         }
     },
+    ElastiCache: {
+        describeCacheClusters: {
+            property: 'CacheClusters',
+            paginate: 'NextToken'
+        }
+    },
     ElasticTranscoder: {
         // TODO: Pagination via NextPageToken and PageToken
         listPipelines: {
